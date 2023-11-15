ab7a0aac899b81af9d6288557fe6dde62e452038
ASERADMIN\ACER <minhpham03112001@gmail.com>
2023-11-15 11:02:58 +0700

    update-count-down

--- javascripts/countdown.js
+++ javascripts/countdown.js
@@ -1,18 +1,17 @@
-var countDownDate = new Date("Nov 10, 2023 23:59:59").getTime();
+var countDownDate = new Date("Jan 10, 2024 23:59:59").getTime();
 
 var x = setInterval(function () {
+	let hide = document.getElementById("countdown");
+	var now = new Date().getTime();
+	var distance = countDownDate - now;
 
-    let hide = document.getElementById("countdown");
-    var now = new Date().getTime();
-    var distance = countDownDate - now;
+	// Time calculations for days, hours, minutes and seconds
+	var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+	var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    let timer = `<div class="itime">
+	let timer = `<div class="itime">
                         <span class="colorBlue">${days}</span> <span class="font-size-16">Ngày</span>
                     </div>
                     <div class="itime">
@@ -23,13 +22,13 @@ var x = setInterval(function () {
                     </div>
                     <div class="itime">
                         <span class="colorBlue">${seconds}</span> <span class="font-size-16">Giây</span>
-                    </div>`
+                    </div>`;
 
-    document.getElementById("timer").innerHTML = timer;
+	document.getElementById("timer").innerHTML = timer;
 
-    // If the count down is over
-    if (distance < 0) {
-        clearInterval(x);
-        hide.classList.add('display-none');
-    }
+	// If the count down is over
+	if (distance < 0) {
+		clearInterval(x);
+		hide.classList.add("display-none");
+	}
 }, 1000);
